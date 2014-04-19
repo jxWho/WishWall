c686d0e63021584007aec4b126fc4b9400698f2c
= <hjxwho@gmail.com>
2014-04-18 22:59:15 -0400

    Create a template
    - wishBox
    Can ues this template to create all wishes

--- WishWall/application/views/templates/mainContent.php
+++ WishWall/application/views/templates/mainContent.php
@@ -1,5 +1,14 @@
     <div id="mainContent">
         <h3>This is main content</h3>
         <hr>
+        <?php
+            $p = array(
+                "wishTitle" => 'My Wish',
+                "wishDescription" => 'I want a keyboard.',
+                "wishMaker" => 'Hack',
+            );
+            for( $i = 0; $i < 2; $i++)
+                $this->load->view('templates/wishBox',$p);
+         ?>
     </div>
 
